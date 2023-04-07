bbf720203679a0762f043f82bf215d23fb4cf15d
Nsher00 <jimmysherwood9@gmail.com>
2023-04-07 16:12:41 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+const App = ()=> {
+    const title = 'Blog Post'
+    const myBody = 'This is my Blog Post'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div className='container'>
+            <h1>{title}</h1>
+            <p>{myBody}</p>
+            
+        </div>
+    )
 }
 
-export default App;
+export default App
